4e675bd50bbea3f6bdc402e8d55b02bdc8347964
Tenshi541 <angelzavaleta541@gmail.com>
2023-02-24 22:05:58 -0800

    password

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
+
 // Assignment code here
 function generatePassword() {
-  console.log( hello);
+  console.log();
+  return //"generated password will go here"
 }
+let hasPasslength
+let hasLower
+let hasUpper
+let hasNumbers
+let hasSpecChars
 
+const passprompt = () => {
+  passlength =parseInt(prompt('how long do you want your password to be?'))
+  hasLower =confirm('do you want lowercase characters?')
+  hasUpper =confirm('do you want Uppercase characters?')
+  hasNumbers =confirm('do you want Numbers?')
+  hasSpecChars =confirm('do you want Spec characters?')
+  console.log(passlength, hasLower, hasUpper, hasNumbers, hasSpecChars)
+}
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -10,6 +25,14 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  
+const pass_el = document.querySelector('#password span');
+const length = 16;
+const chars =
+"ABCDEFGHIJKLMNOPQRSTUVWXYZacdefghijklmnopqrstuvwxyz0123456789!@#$%^&*";
+ let pass = "";
+  for (var i = 0; i <== length; i++){
+}
 
   passwordText.value = password;
 
@@ -17,3 +40,7 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+document.getElementById('generate').addEventListener('click', () => {
+passprompt()
+})
