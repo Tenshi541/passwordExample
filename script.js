c3d2258421b6589b20216a8da1ee5ab1985ca3d4
Tenshi541 <angelzavaleta541@gmail.com>
2023-02-24 22:39:39 -0800

    CHARS

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 
-// Assignment code here
 function generatePassword() {
   console.log();
-  return //"generated password will go here"
+  return// password goes here'i tried :(';
 }
 let hasPasslength
 let hasLower
@@ -18,10 +17,6 @@ const passprompt = () => {
   hasSpecChars =confirm('do you want Spec characters?')
   console.log(passlength, hasLower, hasUpper, hasNumbers, hasSpecChars)
 }
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -31,16 +26,17 @@ const length = 16;
 const chars =
 "ABCDEFGHIJKLMNOPQRSTUVWXYZacdefghijklmnopqrstuvwxyz0123456789!@#$%^&*";
  let pass = "";
-  for (var i = 0; i <== length; i++){
+  for (var i = 0; i <= length; i++){
+let rand = Math.floor(
+  Math.random() * chars.length
+  );
+pass += chars.substring(rand, rand +1);
+  }
+  pass_el.innerText = pass;
+  generatePassword();
+
 }
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
 document.getElementById('generate').addEventListener('click', () => {
 passprompt()
 })
+  
